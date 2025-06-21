5a6f00dadd3e08b065ad1dda79b59fd4bca8d4eb
Kristian Koome <originalkristiann@gmail.com>
2025-06-21 11:15:56 +0300

    NodeJS Mern

---
+++ Routes/studentroute.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const routes = express.Router();
+
+//get a list of students from the database 
+routes.get("/students",(req,res)=> {
+    res.send({type:"Get Request"});
+});
+
+//add student to the database
+routes.post("/students",(req,res)=>{
+    res.send({type:"Post Request"});
+});
+
+//update student in the database
+routes.put("/students/:id",(req,res)=>{
+    res.send({type:"Update Request"});
+});
+
+//delete student from the database 
+routes.delete("/student/:id",(req,res)=>{
+    res.send({type:"Delete Request"});
+});
+
+module.exports = routes;
